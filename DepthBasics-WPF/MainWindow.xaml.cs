7ad8c54820505dc48d742268b340f97c9ac47696
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-07-17 12:08:41 -0400

    changes

--- DepthBasics-WPF/MainWindow.xaml.cs
+++ DepthBasics-WPF/MainWindow.xaml.cs
@@ -70,10 +70,6 @@ namespace Microsoft.Samples.Kinect.DepthBasics
         /// <param name="e">event arguments</param>
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
-            // Look through all sensors and start the first connected one.
-            // This requires that a Kinect is connected at the time of app startup.
-            // To make your app robust against plug/unplug, 
-            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
             foreach (var potentialSensor in KinectSensor.KinectSensors)
             {
                 if (potentialSensor.Status == KinectStatus.Connected)
@@ -85,25 +81,18 @@ namespace Microsoft.Samples.Kinect.DepthBasics
 
             if (null != this.sensor)
             {
-                // Turn on the depth stream to receive depth frames
                 this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
                 
-                // Allocate space to put the depth pixels we'll receive
                 this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
 
-                // Allocate space to put the color pixels we'll create
                 this.colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
 
-                // This is the bitmap we'll display on-screen
                 this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
 
-                // Set the image we display to point to the bitmap where we'll put the image data
                 this.Image.Source = this.colorBitmap;
 
-                // Add an event handler to be called whenever there is new depth frame data
                 this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
 
-                // Start the sensor!
                 try
                 {
                     this.sensor.Start();
@@ -144,14 +133,11 @@ namespace Microsoft.Samples.Kinect.DepthBasics
             {
                 if (depthFrame != null)
                 {
-                    // Copy the pixel data from the image to a temporary array
                     depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
 
-                    // Get the min and max reliable depth for the current frame
                     int minDepth = depthFrame.MinDepth;
                     int maxDepth = depthFrame.MaxDepth;
 
-                    // Convert the depth to RGB
                     int colorPixelIndex = 0;
                     for (int i = 0; i < this.depthPixels.Length; ++i)
                     { 
@@ -159,21 +145,15 @@ namespace Microsoft.Samples.Kinect.DepthBasics
 
                         byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
 
-                        // Write out blue byte
                         this.colorPixels[colorPixelIndex++] = intensity;
 
-                        // Write out green byte
                         this.colorPixels[colorPixelIndex++] = intensity;
-
-                        // Write out red byte                        
+             
                         this.colorPixels[colorPixelIndex++] = intensity;
                                                 
-                        // We're outputting BGR, the last byte in the 32 bits is unused so skip it
-                        // If we were outputting BGRA, we would write alpha here.
                         ++colorPixelIndex;
                     }
 
-                    // Write the pixel data into our bitmap
                     this.colorBitmap.WritePixels(
                         new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
                         this.colorPixels,
@@ -238,10 +218,8 @@ namespace Microsoft.Samples.Kinect.DepthBasics
                 return;
             }
 
-            // create a png bitmap encoder which knows how to save a .png file
             BitmapEncoder encoder = new PngBitmapEncoder();
 
-            // create frame from the writable bitmap and add to encoder
             encoder.Frames.Add(BitmapFrame.Create(this.colorBitmap));
 
             string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
@@ -250,7 +228,6 @@ namespace Microsoft.Samples.Kinect.DepthBasics
 
             string path = Path.Combine(myPhotos, "KinectSnapshot-" + time + ".png");
 
-            // write the new file to disk
             try
             {
                 using (FileStream fs = new FileStream(path, FileMode.Create))
