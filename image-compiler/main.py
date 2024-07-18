eda00193bef0d69133e6cd93774ae154241354b6
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-07-18 00:07:32 -0400

    image processing

--- image-compiler/main.py
+++ image-compiler/main.py
@@ -1,17 +1,39 @@
-import json, os
+import json, os, numpy as np
 import cv2
 
 cfg = json.load(open('config.json',))
-print(cfg)
+# print(cfg)
 
-files = os.listdir(cfg['depth-frames-folder'])
-image = cv2.imread(f"{cfg['depth-frames-folder']}/{files[-1]}")
+color_map = []
+
+color_data = cfg['color-data']
+color_grayscale = color_data['grayscale']
+color_bgr = color_data['bgr']
+
+for depth in range(256):
+    for key in color_grayscale:
+        if depth <  color_grayscale[key]: 
+            color_map.append(color_bgr[key])
+            break
+
+print(color_map)
+
+files = os.listdir(cfg['depth-frames-dir'])
+image = cv2.imread(f"{cfg['depth-frames-dir']}/{files[-1]}")
 h, w, c = image.shape
+b, g, r = np.zeros((h,w)), np.zeros((h,w)), np.zeros((h,w))
+
 print(h,w,c)
 for y in range(h):
     for x in range(w):
-        print(image[y][x][0],end=" ")
+        color = color_map[image[y][x][0]]
+        b[y,x] = color[0]
+        g[y,x] = color[1]
+        r[y,x] = color[2]
+        print(color_map[image[y][x][0]],end=" ")
     print("")
 
+final_img = cv2.merge([b,g,r])
+cv2.imwrite(f"{cfg['output-dir']}/final.png",final_img)
 
 print("welcome to image-compiler")
