e8dd906d4f72d38f2b9922a9d4bc220800beacbf
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-07-17 23:16:28 -0400

    reading image data

--- image-compiler/main.py
+++ image-compiler/main.py
@@ -1,1 +1,17 @@
+import json, os
+import cv2
+
+cfg = json.load(open('config.json',))
+print(cfg)
+
+files = os.listdir(cfg['depth-frames-folder'])
+image = cv2.imread(f"{cfg['depth-frames-folder']}/{files[-1]}")
+h, w, c = image.shape
+print(h,w,c)
+for y in range(h):
+    for x in range(w):
+        print(image[y][x][0],end=" ")
+    print("")
+
+
 print("welcome to image-compiler")
