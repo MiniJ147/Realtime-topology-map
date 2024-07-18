a6831abf00e428594bbf9717b4100975a78a4622
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-07-18 00:46:57 -0400

    refactor

--- image-compiler/main.py
+++ image-compiler/main.py
@@ -1,39 +1,54 @@
 import json, os, numpy as np
 import cv2
 
-cfg = json.load(open('config.json',))
-# print(cfg)
+# bgr constants
+BLUE = 0
+GREEN = 1
+RED = 2
 
-color_map = []
+class Image_Compiler:
+    color_map = []
+    def __init__(self):
+        self.load_config()
+        self.load_color_map()
 
-color_data = cfg['color-data']
-color_grayscale = color_data['grayscale']
-color_bgr = color_data['bgr']
+    def load_config(self):
+        self.cfg = json.load(open('config.json'))
+        self.color_grayscale = self.cfg['color-data']['grayscale']
+        self.color_bgr = self.cfg['color-data']['bgr']
 
-for depth in range(256):
-    for key in color_grayscale:
-        if depth <  color_grayscale[key]: 
-            color_map.append(color_bgr[key])
-            break
+        self.output_dir = self.cfg['output-dir']
+        self.input_dir = self.cfg['input-dir']
+    
+    def load_color_map(self):
+         for depth in range(256):
+            for key in self.color_grayscale:
+                if depth < self.color_grayscale[key]:
+                    self.color_map.append(self.color_bgr[key])
+                    break
 
-print(color_map)
+    def compile_image(self,file_name: str):
+        # input data
+        input_img = cv2.imread(f"{self.input_dir}/{file_name}")
+        h, w, c = input_img.shape
+        
+        # output img channels
+        b,g,r = np.zeros((h,w)), np.zeros((h,w)), np.zeros((h,w))
 
-files = os.listdir(cfg['depth-frames-dir'])
-image = cv2.imread(f"{cfg['depth-frames-dir']}/{files[-1]}")
-h, w, c = image.shape
-b, g, r = np.zeros((h,w)), np.zeros((h,w)), np.zeros((h,w))
+        for y in range(h):
+            for x in range(w):
+                depth_val = input_img[y][x][0]
+                bgr = self.color_map[depth_val]
 
-print(h,w,c)
-for y in range(h):
-    for x in range(w):
-        color = color_map[image[y][x][0]]
-        b[y,x] = color[0]
-        g[y,x] = color[1]
-        r[y,x] = color[2]
-        print(color_map[image[y][x][0]],end=" ")
-    print("")
+                # setting channels
+                b[y,x] = bgr[BLUE]
+                g[y,x] = bgr[GREEN]
+                r[y,x] = bgr[RED]
+        
+        out_img = cv2.merge([b,g,r])
+        cv2.imwrite(f"{self.output_dir}/final.png",out_img)
 
-final_img = cv2.merge([b,g,r])
-cv2.imwrite(f"{cfg['output-dir']}/final.png",final_img)
 
 print("welcome to image-compiler")
+comp = Image_Compiler()
+comp.compile_image('frame-1721277859.png')
